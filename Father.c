8359d086e3681afcb994dc15b98abceec068350b
tshinhar <tshinhar@gmail.com>
2020-11-06 21:47:39 +0200

    Add files via upload

--- Father.c
+++ Father.c
@@ -6,14 +6,14 @@ int main(int argc, char* argv[])
 		printf("Not enough arguments.\n");
 		exit(1);
 	}
-	FILE* file_pointer = fopen(argv[1], "r");
-	if (file_pointer == NULL) {
+	FILE* file_pointer = NULL;
+	if (fopen_s(&file_pointer, argv[1], "r") != 0) {
 		printf("couldn't read file");
 		exit(1);
 	}
-	int num_of_gens, forest_size;
-	forest_size = fgets(forest_size, 1, file_pointer)+60;
-	num_of_gens = fgets(num_of_gens, 1, file_pointer)+60;
+	int num_of_gens = 0, forest_size = 0;
+	forest_size = (int)fgets(forest_size, 1, file_pointer) + 60;
+	num_of_gens = (int)fgets(num_of_gens, 1, file_pointer) + 60;
 	int string_size = forest_size^2;
 	
 
@@ -27,8 +27,8 @@ char *get_forest(int forest_size,  FILE* file_pointer ) {
 	do
 	{
 		current_char = (char)fgetc(file_pointer);
-		if(current_char != ',')
-			forest[i] = current_char
+		if (current_char != ',')
+			forest[i] = current_char;
 
 	} while (file_pointer != EOF);
 	return forest;
