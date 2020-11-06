1ef482eee549c3473e0114ba4c040201e9d20a6b
yaeli30994 <74063406+yaeli30994@users.noreply.github.com>
2020-11-06 18:21:40 +0200

    Update son.c

--- son.c
+++ son.c
@@ -2,17 +2,28 @@
 
 int main(int argc, char* argv[])
 {
-	return get_burned_trees_num(argv[1], 'F');
+	//check if there are sufficiante amount of arguments
+	if (argc == 2) 
+		printf("The string (forest) supplied is %s\n", argv[1]);
+	else if (argc > 2)
+		printf("Too many arguments supplied.\n");
+	else
+		printf("One argument expected (program's name).\n");
+	exit(get_num_of_cell_type(argv[1], 'F'));
+	
+	//for father- check what to do it the input is not a square
+	//int s = sqrt(forrest_size);
+	//if ((s * s) == forrest_s[i]) //if true- they forest is square
 }
 
 
-int get_burned_trees_num(char *string, char value)
+int get_num_of_cell_type(char *forest, char cell_type)
 {
 	int counter = 0, i = 0;
-    char *pointer = string;
+    char *pointer = forest;
     while (pointer[i] != NULL)
     {
-        if (pointer[i] == value)
+        if (pointer[i] == cell_type)
             counter++;
         i++;
     }
