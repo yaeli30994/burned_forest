d152e748dbd16c6542ef18b3f8f9cc84cbd72c53
yaeli30994 <74063406+yaeli30994@users.noreply.github.com>
2020-11-08 21:44:44 +0200

    Update Father.h

--- Father.h
+++ Father.h
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <windows.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define SUCCESS_CODE ((int)(0))
 #define FAILURE_CODE ((int)(-1))
@@ -12,4 +14,11 @@ int call_process(char* command_line);
 int get_num_of_burned(char* forest);
 int write_to_file(FILE* file_ptr, char* line);
 int create_output_file(char* forest, int num_of_gens);
+char* forest_next_step(int forest_size, char* forest_string);
+void Get_neighbours(int i, char* forest_string, int forest_size, char* neighbours_total, char* neighbours_plus_shape, int* num_total, int* num_plus);
+bool top_row(int i, int forest_size);
+bool bottom_row(int i, int forest_size);
+bool left_column(int i, int forest_size);
+bool right_column(int i, int forest_size);
+void Check_neighbours_and_update_cell(int i, char* forest_string, char* neighbours_total, char* neighbours_plus_shape, int* num_total, int* num_plus, char* next_step_forest);
 #pragma once
